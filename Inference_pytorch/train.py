5d3b0d040ccb2efe82d4bbacd3aa500e3772ef8a
chungen04 <b09901027@ntu.edu.tw>
2022-11-22 01:59:48 +0000

    Inferenced VGG8

--- Inference_pytorch/train.py
+++ Inference_pytorch/train.py
@@ -17,7 +17,7 @@ parser.add_argument('--dataset', default='cifar10', help='cifar10|cifar100|image
 parser.add_argument('--model', default='VGG8', help='VGG8|DenseNet40|ResNet18')
 parser.add_argument('--mode', default='WAGE', help='WAGE|FP')
 parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=200, help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 10)')
 parser.add_argument('--grad_scale', type=float, default=8, help='learning rate for wage delta calculation')
 parser.add_argument('--seed', type=int, default=117, help='random seed (default: 1)')
 parser.add_argument('--log_interval', type=int, default=100,  help='how many batches to wait before logging training status')
